e81aa28b937022f132e50d169ad2e7322b2dc657
sasasan03 <sako.hiro62@gmail.com>
2023-03-13 00:05:45 +0900

    QiitaRowViewを作成

---
+++ QiitaAPISwiftUI/View/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  QiitaAPISwiftUI
+//
+//  Created by sako0602 on 2023/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+//    @ObservedObject var qiita = QiitaViewModel()
+
+    @StateObject var qiita = QiitaViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            List(qiita.articles) { article in
+                QiitaRowView(article: article)
+            }
+            .navigationTitle("Qiitaの記事")
+        }
+        .task {//画面が非表示となった時に task で実行されている処理が自動的にキャンセルされる。
+            //そのため、画面が再表示されたときに処理結果が重複することを避けることができる。←onapperとの違い
+                await qiita.fetchQiitaArticles()
+            }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QiitaAPISwiftUI/View/QiitaRowView.swift
@@ -0,0 +1,40 @@
+//
+//  QiitaRowView.swift
+//  QiitaAPISwiftUI
+//
+//  Created by sako0602 on 2023/03/11.
+//
+
+import SwiftUI
+
+struct QiitaRowView: View {
+    
+    @State var article: QiitaArticle
+    
+    var body: some View {
+        NavigationStack {
+            HStack{
+                Link(article.title, destination: article.url)
+                Spacer()
+                AsyncImage(url: article.user.profileImageURL){ image in
+                    image.resizable().scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 100)
+            }
+        }
+    }
+}
+
+//struct QiitaRowView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QiitaRowView(article: QiitaArticle(
+//            title:"qiitaの記事",
+//            createdAt: "2023/3/12",
+//            url: (URL(string: "https://qiita.com/goto2023/items/08c5c724a39a78a25d70"),
+//            user: User.init(name: T##String, profileImageURL: T##URL)
+//
+//        ))
+//    }
+//}
